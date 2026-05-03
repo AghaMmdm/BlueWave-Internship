774e3cb4a3618ae201e6e809c9434475e5be3806
User <mrahimiwork83@gmail.com>
2026-05-03 10:50:37 +0330

    one digit calculator with key pad

--- codes/Key_pad_sensor.py
+++ codes/Key_pad_sensor.py
@@ -13,6 +13,8 @@ keys = [
     ['A', 'B', 'C', 'D']
 ]
 
+nums = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0']
+
 def scan():
     for j, col in enumerate(cols):
         col.value(1)
@@ -20,8 +22,49 @@ def scan():
             if row.value() == 1:
                 print("Pressed:", keys[i][j])
                 time.sleep(0.3)
+                return keys[i][j]
         col.value(0)
 
+num1 = 0
+num2 = 0
+
 while True:
-    scan()
+    temp = scan()
+    if temp in nums:
+        if num1 == 0:
+            num1 = int(temp)
+        else:
+            num2 = int(temp)
+            
+    if temp == 'A':
+        print("Sum:", num1 + num2)
+        num1 = 0
+        num2 = 0
+        
+    if temp == 'B':
+        print("Difference:", num1 - num2)
+        num1 = 0
+        num2 = 0
+        
+    if temp == 'C':
+        print("Product:", num1 * num2)
+        num1 = 0
+        num2 = 0
+        
+    if temp == 'D':
+        if num2 != 0:
+            print("Quotient:", num1 / num2)
+        else:
+            print("Error: Division by zero")
+        num1 = 0
+        num2 = 0
+        
+    if temp == '*':
+        print("Cleared")
+        num1 = 0
+        num2 = 0
+        
+    if temp == '#':
+        print("Exiting")
+        break
 
